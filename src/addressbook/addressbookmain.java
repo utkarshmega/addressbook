7fd95431ec9a13c3b7f0419becc57214fc20ac47
utkarshmega <utkarsheduc@gmail.com>
2020-09-26 11:14:24 +0530

    master branch task done

--- src/addressbook/addressbookmain.java
+++ src/addressbook/addressbookmain.java
@@ -1,5 +1,13 @@
 package addressbook;
 
 public class addressbookmain {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Address Book\r\n"
+				+ "Program in\r\n"
+				+ "AddressBookMain class \r\n"
+		         );
+	}
 
 }
