bb5c9e86d3a6a11a49b2d55ed85b4c9631516cb1
utkarshmega <utkarsheduc@gmail.com>
2020-09-26 16:30:40 +0530

    UC3 completed

--- src/addressbook/addressbookcontent.java
+++ src/addressbook/addressbookcontent.java
@@ -20,6 +20,61 @@ public class addressbookcontent {
 		this.zip = zip;
 		this.phNo = mobNo;
 		this.email = mail;
-	}	
-
+	}
+	public String getFirstName()
+	{
+		return firstName;
+	}
+	public String getLasttName()
+	{
+		return lastName;
+	}
+	public String getAddress()
+	{
+		return address;
+	}
+	public String getCity()
+	{
+		return city;
+	}
+	public int getZip()
+	{
+		return zip;
+	}
+	public String getPhoneNumber()
+	{
+		return phNo;
+	}
+	public String getEmail()
+	{
+		return email;
+	}
+	public void setFirstName(String fname)
+	{
+		this.firstName = fname;
+	}
+	public void setLastName(String lname)
+	{
+		this.lastName = lname;
+	}
+	public void setAddress(String addr)
+	{
+		this.address = addr;
+	}
+	public void setCity(String city)
+	{
+		this.city = city;
+	}
+	public void setZip(int zipc)
+	{
+		this.zip = zipc;
+	}
+	public void setPhoneNumber(String phn)
+	{
+		this.phNo = phn;
+	}
+	public void setEmail(String mail)
+	{
+		this.email = mail;
+	}
 }
